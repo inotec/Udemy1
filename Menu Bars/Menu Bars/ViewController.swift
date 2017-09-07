816f13733c49f06bcacf2d27d341ba3eb8699965
inotec <inotec@ieee.org>
2017-09-07 08:57:26 -0400

    NavBar

---
+++ Menu Bars/Menu Bars/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by Barry Keane on 9/7/17.
+//  Copyright © 2017 Inotec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  
+  @IBAction func camButt(_ sender: Any) {
+    
+    print ("Camera Butt Pressed")
+    
+  }
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
