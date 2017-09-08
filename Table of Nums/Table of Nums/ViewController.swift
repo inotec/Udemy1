73f083f181297b69a2402b7e45a9728572874b82
inotec <inotec@ieee.org>
2017-09-08 08:31:51 -0400

    50 row table view

---
+++ Table of Nums/Table of Nums/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Table of Nums
+//
+//  Created by Barry Keane on 9/7/17.
+//  Copyright © 2017 Inotec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+  internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
+    
+    return 50
+    
+  }
+  
+  
+  internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell  {
+    
+    
+    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+    
+    cell.textLabel?.text = String(indexPath.row + 1)
+    
+    return cell
+    
+    
+  }
+
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
