c438b0e2834486c15407125ca82fab0b1b1a3a0d
inotec <inotec@ieee.org>
2017-09-11 12:15:13 -0400

    Animations2

--- 16Animations/Animations/ViewController.swift
+++ 16Animations/Animations/ViewController.swift
@@ -10,23 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  // Outlets:
+  
+  @IBOutlet weak var button: UIButton!
   
   @IBOutlet weak var image: UIImageView!
   
   var counter = 00
   
-  @IBAction func next(_ sender: Any)
+  var timer = Timer()
+  
+  var running = false
+    
+  func animate()
   {
-    image.image = UIImage(named: ("frame_" + String(format: "%02d", counter) + "_delay-0.1s.gif"))
-    
-    counter += 1
-    
-    if counter > 15
+    if running
     {
-      counter = 0
+      image.image = UIImage(named: ("frame_" + String(format: "%02d", counter) + "_delay-0.1s.gif"))
+      
+      counter += 1
+      
+      if counter > 15
+      {
+        counter = 0
+      }
     }
   }
   
+  // Actions:
+
+  @IBAction func fadIn(_ sender: Any)
+  {
+    image.alpha = 0   // make invisible initially
+    UIView.animate(withDuration: 3, animations:  // Graduallty fade in
+    {
+      self.image.alpha = 1
+    })
+  }
+  
+  @IBAction func slideIn(_ sender: Any)
+  {
+    
+    image.center = CGPoint(x: image.center.x - 500, y: image.center.y)
+    
+    UIView.animate(withDuration: 3, animations:
+    {
+      // NOTE: "Any time you're inside a closure you need self to refer to object (ViewController) outside closure
+      self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y)
+    })
+    
+  }
+  
+  
+  @IBAction func grow(_ sender: Any)
+  {
+    image.frame = CGRect(x: 0, y: self.image.center.y, width: 0, height: 0)
+    
+    UIView.animate(withDuration: 3)
+    {
+      self.image.frame = CGRect(x: 0, y: self.image.center.y - 190, width: 380, height: 380)
+    }
+  }
+  
+  @IBAction func next(_ sender: Any)
+  {
+    // Toggle running
+    running = !running
+    
+    // Update button label
+    if running
+    {
+      button.setTitle("Stop Animation", for: [])  // empty brackets for controller state...I don't know why
+    }
+    else
+    {
+      button.setTitle("Start Animation", for: [])
+    }
+  
+    timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
+    
+  }
   
   
 
